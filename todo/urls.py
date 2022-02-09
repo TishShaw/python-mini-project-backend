73bac08b16fbf0dba96888c749a6d074ccc8e5a6
Tishhshaw <Tish.shaw@yahoo.com>
2022-02-09 12:58:43 -0500

    add todo url routes

---
+++ todo/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    
+    path('todo/', views.TodoList.as_view(), name='todo_list'),
+    path('todo/<int:pk>', views.TodoDetail.as_view(), name='todo_detail'),
+    
+]
